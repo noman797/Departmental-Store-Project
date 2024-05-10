e93404327dd88cdfb15fd72cea9439e12c41ccf0
Abdullah Al Noman <165512794+noman797@users.noreply.github.com>
2024-05-10 11:54:08 +0600

    project.c

--- project.c
+++ project.c
@@ -14,215 +14,14 @@ typedef struct item
     int quantity;
     int reserved;
     struct item *next;
-}item;
+} item;
 
-item *head=NULL;
+item *head = NULL;
 
 void clearScreen()
 {
-    system("cls");
+    system("clear");
 }
-
-void addNewItem()
-{
-    clearScreen();
-
-    item* newItem=(item*)malloc(sizeof(item));
-
-    printf("Adding a new item...\n");
-    printf("Enter Item Name: ");
-    scanf("%s",newItem->name);
-    printf("Enter Item Code: ");
-    scanf("%s",newItem->code);
-    printf("Enter Unit Price: ");
-    scanf("%f",&newItem->unitPrice);
-    printf("Enter Item Quantity: ");
-    scanf("%d",&newItem->quantity);
-
-    newItem->reserved=0;
-    newItem->next=head;
-    head=newItem;
-
-    FILE* file=fopen("items.txt", "a");
-    if (file==NULL) {
-        printf("Error opening file.\n");
-        free(newItem);
-        return;
-    }
-
-    fprintf(file,"%s %s %.2f %d\n", newItem->name,newItem->code,newItem->unitPrice,newItem->quantity);
-    fclose(file);
-
-    printf("Item added successfully!\n");
-    printf("Press any key to continue...\n");
-    getchar();
-}
-
-void updateFile()
-{
-    FILE* file=fopen("items.txt", "w");
-    if (file==NULL)
-    {
-        printf("Error opening file.\n");
-        return;
-    }
-
-    item* current=head;
-    while(current!=NULL)
-    {
-        fprintf(file,"%s %s %.2f %d\n",current->name,current->code,current->unitPrice,current->quantity);
-        current=current->next;
-    }
-
-    fclose(file);
-}
-
-void printBillSlip(float total)
-{
-    clearScreen();
-
-    printf("------------\n");
-    printf("TOTAL AMOUNT: Rs. %.2f\n", total);
-    printf("Thank you for shopping with us!\n");
-    printf("------------\n");
-
-
-    item* current=head;
-    while(current!=NULL)
-    {
-        current->quantity-=current->reserved;
-        current->reserved=0;
-        current=current->next;
-    }
-
-
-    updateFile();
-
-    printf("Press any key to continue...\n");
-    getchar();
-
-}
-void calculateBill() 
-{
-    clearScreen();
-    char code[MAX_CODE];
-    int quantity;
-    float total = 0;
-    char choice;
-
-    printf("Calculating the bill...\n");
-
-    do 
-    {
-        printf("Enter Item Code: ");
-        scanf("%s", code);
-
-        printf("Enter Quantity: ");
-        scanf("%d", &quantity);
-
-        item *current = head;
-        int found = 0;
-
-        while (current != NULL)
-        {
-            if (strcmp(current->code, code) == 0) 
-            {
-                found = 1;
-                if (current->quantity - current->reserved >= quantity) {
-                    total += current->unitPrice * quantity;
-                    current->reserved += quantity;
-                } 
-                else 
-                {
-                    printf("Not enough quantity available for %s.\n", current->name);
-                }
-                break;
-            }
-            current = current->next;
-        }
-
-        if (found==0) 
-        {
-            printf("Item with code %s not found!\n", code);
-        }
-
-        printf("Do you want to add another item? (y/n): ");
-        scanf(" %c", &choice);
-
-    } while (choice=='y');
-
-    printBillSlip(total);
-    printf("Press any key to continue...\n");
-    getchar();
-}
-void displayAllItems()
-{
-    clearScreen();
-    printf("Displaying all items...\n");
-    printf("Name\t\tCode\tUnit Price\tQuantity\n");
-    printf("--------------------------------------------\n");
-
-    item* current=head;
-    while (current!=NULL)
-    {
-        printf("%s\t\t%s\t%.2f\t%d\n",current->name,current->code,current->unitPrice,current->quantity);
-        current=current->next;
-    }
-
-    printf("Press any key to continue...\n");
-    getchar();
-
-}
-
-void search()
- {
-    clearScreen();
-    int choice;
-    char searchKey[50];
-
-    printf("Search items by:\n");
-    printf("1. Code\n");
-    printf("2. Name\n");
-    printf("Enter your choice: ");
-    scanf("%d", &choice);
-
-    if (choice != 1 && choice != 2) 
-    {
-        printf("Invalid choice.\n");
-        return;
-    }
-
-    printf("Enter search key: ");
-    scanf("%s", searchKey);
-
-    item* current = head;
-    int found = 0;
-
-    printf("Search Results:\n");
-    printf("Name\t\tCode\tUnit Price\tQuantity\n");
-    printf("--------------------------------------------\n");
-
-    while (current != NULL) {
-        if ((choice == 1 && strcmp(current->code, searchKey) == 0) ||
-            (choice == 2 && strcmp(current->name, searchKey) == 0)) 
-        {
-            printf("%s\t\t%s\t%.2f\t\t%d\n",current->name, current->code, current->unitPrice, current->quantity);
-            found = 1;
-        }
-        current = current->next;
-    }
-
-    if (found == 0) 
-    {
-        printf("No items found\n");
-    }
-
-    printf("Press any key to continue...\n");
-    getchar(); 
-    getchar(); 
-    
-}
-
 
 bool authenticate()
 {
@@ -237,7 +36,7 @@ bool authenticate()
     printf("                                                    Enter password: ");
     scanf("%s", password);
 
-    if (strcmp(username,"admin") == 0 && strcmp(password,"1234") == 0)
+    if (strcmp(username, "admin") == 0 && strcmp(password, "1234") == 0)
     {
         return true;
     }
@@ -248,6 +47,381 @@ bool authenticate()
     }
 }
 
+void addNewItem()
+{
+    clearScreen();
+
+    item *newItem = (item *)malloc(sizeof(item));
+
+    printf("Adding a new item...\n");
+    printf("Enter Item Name: ");
+    scanf("%s", newItem->name);
+    printf("Enter Item Code: ");
+    scanf("%s", newItem->code);
+    printf("Enter Unit Price: ");
+    scanf("%f", &newItem->unitPrice);
+    printf("Enter Item Quantity: ");
+    scanf("%d", &newItem->quantity);
+
+    newItem->reserved = 0;
+    newItem->next = head;
+    head = newItem;
+
+    FILE *file = fopen("items.txt", "a");
+    if (file == NULL)
+    {
+        printf("Error opening file.\n");
+        free(newItem);
+        return;
+    }
+
+    fprintf(file, "%s %s %.2f %d\n", newItem->name, newItem->code, newItem->unitPrice, newItem->quantity);
+    fclose(file);
+
+    printf("Item added successfully!\n");
+    printf("Press any key to continue...\n");
+    getchar();
+}
+
+void editItem()
+{
+    clearScreen();
+    int option;
+    char search[MAX_CODE];
+    printf("Editing an existing item...\n");
+    printf("Edit all details by\n");
+    printf("1. Name\n");
+    printf("2. Code\n");
+    printf("\n");
+    printf("Enter your choice: ");
+    scanf("%d", &option);
+
+    if (option != 1 && option != 2)
+    {
+        printf("Invalid choice!\n");
+        printf("Press any key to continue...\n");
+        getchar(); 
+        return;
+    }
+
+    printf("Enter search key (name or code): ");
+    getchar(); 
+    fgets(search, sizeof(search), stdin);
+    search[strcspn(search, "\n")] = '\0'; 
+
+    int found = 0;
+
+    item *current = head;
+    while (current != NULL)
+    {
+        if ((option == 1 && strcmp(current->name, search) == 0) ||
+            (option == 2 && strcmp(current->code, search) == 0))
+        {
+            found = 1;
+            
+                printf("Item found! Enter updated details:\n");
+                printf("Enter New Name: ");
+                scanf("%s", current->name);
+                printf("Enter New Code: ");
+                scanf("%s", current->code);
+                printf("Enter Unit Price: ");
+                scanf("%f", &current->unitPrice);
+                printf("Enter Item Quantity: ");
+                scanf("%d", &current->quantity);
+                printf("Item updated successfully!\n");
+            
+            printf("Press any key to continue...\n");
+            getchar(); 
+            break;
+        }
+        current = current->next;
+    }
+
+    if (found==0)
+    {
+        printf("Item is not found!\n");
+    }
+    printf("Press any key to continue...\n");
+    getchar(); 
+}
+
+void deleteItem()
+{
+    clearScreen();
+    int option;
+    char search[MAX_CODE];
+    printf("Deleting an existing item...\n");
+    printf("Choose what to delete:\n");
+    printf("1. Name\n");
+    printf("2. Code\n");
+    printf("Enter your choice: ");
+    scanf("%d", &option);
+
+    if (option != 1 && option != 2)
+    {
+        printf("Invalid choice!\n");
+        printf("Press Enter to continue...\n");
+        getchar();
+        return;
+    }
+
+    printf("Enter search key: ");
+    scanf("%s", search);
+
+    int found = 0;
+
+    item *current = head;
+    item *prev = NULL;
+    while (current != NULL)
+    {
+        if ((option == 1 && strcmp(current->name, search) == 0) ||
+            (option == 2 && strcmp(current->code, search) == 0))
+        {
+            found = 1;
+
+            if (prev == NULL)
+            {
+                head = current->next;
+            }
+            else
+            {
+                prev->next = current->next;
+            }
+
+            printf("Item with code %s deleted successfully!\n", current->code);
+            free(current);
+            printf("Press Enter to continue...\n");
+            getchar();
+            break;
+        }
+        prev = current;
+        current = current->next;
+    }
+
+    if (found == 0)
+    {
+        printf("Item is not found!\n");
+        printf("Press Enter to continue...\n");
+        getchar();
+    }
+}
+
+void calculateBill()
+{
+    clearScreen();
+    char code[MAX_CODE];
+    char name[MAX_NAME];
+    int quantity;
+    float total = 0;
+    char choice;
+
+    printf("Calculating the bill...\n");
+
+    do
+    {
+        printf("Search items by:\n");
+        printf("1. Code\n");
+        printf("2. Name\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice)
+        {
+        case 1:
+            printf("Enter Item Code: ");
+            scanf("%s", code);
+            break;
+        case 2:
+            printf("Enter Item Name: ");
+            scanf("%s", name);
+            break;
+        default:
+            printf("Invalid choice.\n");
+            continue;
+        }
+
+        printf("Enter Quantity: ");
+        scanf("%d", &quantity);
+
+        item *current = head;
+        int found = 0;
+
+        while (current != NULL)
+        {
+            if ((choice == 1 && strcmp(current->code, code) == 0) ||
+                (choice == 2 && strcmp(current->name, name) == 0))
+            {
+                found = 1;
+                if (current->quantity - current->reserved >= quantity)
+                {
+                    total += current->unitPrice * quantity;
+                    current->reserved += quantity;
+                }
+                else
+                {
+                    printf("Not enough quantity available for %s.\n", current->name);
+                }
+                break;
+            }
+            current = current->next;
+        }
+
+        if (found == 0)
+        {
+            if (choice == 1)
+            {
+                printf("Item with code %s not found!\n", code);
+            }
+            else
+            {
+                printf("Item with name %s not found!\n", code);
+            }
+        }
+
+        printf("Do you want to add another item? (y/n): ");
+        scanf(" %c", &choice);
+
+    } while (choice == 'y');
+
+    printf("Press any key to continue...\n");
+    getchar(); 
+    printBillSlip(total);
+}
+
+void displayAllItems()
+{
+    clearScreen();
+    printf("Displaying all items...\n");
+    printf("Name\t\tCode\tUnit Price\tQuantity\n");
+    printf("--------------------------------------------\n");
+
+    item *current = head;
+    while (current != NULL)
+    {
+        printf("%s\t\t%s\t%.2f\t%d\n", current->name, current->code, current->unitPrice, current->quantity);
+        current = current->next;
+    }
+
+    printf("Press any key to continue...\n");
+    getchar();
+}
+
+void search()
+{
+    clearScreen();
+    int choice;
+    char searchKey[50];
+
+    printf("Search items by:\n");
+    printf("1. Code\n");
+    printf("2. Name\n");
+    printf("Enter your choice: ");
+    scanf("%d", &choice);
+
+    if (choice != 1 && choice != 2)
+    {
+        printf("Invalid choice.\n");
+        return;
+    }
+
+    printf("Enter search key: ");
+    scanf("%s", searchKey);
+
+    item *current = head;
+    int found = 0;
+
+    printf("Search Results:\n");
+    printf("Name\t\tCode\tUnit Price\tQuantity\n");
+    printf("--------------------------------------------\n");
+
+    while (current != NULL)
+    {
+        if ((choice == 1 && strcmp(current->code, searchKey) == 0) ||
+            (choice == 2 && strcmp(current->name, searchKey) == 0))
+        {
+            printf("%s\t\t%s\t%.2f\t\t%d\n", current->name, current->code, current->unitPrice, current->quantity);
+            found = 1;
+        }
+        current = current->next;
+    }
+
+    if (found == 0)
+    {
+        printf("No items found\n");
+    }
+
+    printf("Press any key to continue...\n");
+    getchar();
+}
+
+void savefile()
+{
+    FILE *file = fopen("items.txt", "r");
+    if (file == NULL)
+    {
+        printf("Error opening file.\n");
+        return;
+    }
+
+    item newItem;
+
+    while (fscanf(file, "%s %s %f %d", newItem.name, newItem.code, &newItem.unitPrice, &newItem.quantity) == 4)
+    {
+        item *newItemPtr = (item *)malloc(sizeof(item));
+
+        strcpy(newItemPtr->name, newItem.name);
+        strcpy(newItemPtr->code, newItem.code);
+        newItemPtr->unitPrice = newItem.unitPrice;
+        newItemPtr->quantity = newItem.quantity;
+        newItemPtr->reserved = 0;
+        newItemPtr->next = head;
+        head = newItemPtr;
+    }
+
+    fclose(file);
+}
+
+void updateFile()
+{
+    FILE *file = fopen("items.txt", "w");
+    if (file == NULL)
+    {
+        printf("Error opening file.\n");
+        return;
+    }
+
+    item *current = head;
+    while (current != NULL)
+    {
+        fprintf(file, "%s %s %.2f %d\n", current->name, current->code, current->unitPrice, current->quantity);
+        current = current->next;
+    }
+
+    fclose(file);
+}
+
+void printBillSlip(float total)
+{
+    clearScreen();
+
+    printf("------------\n");
+    printf("TOTAL AMOUNT: Rs. %.2f\n", total);
+    printf("Thank you for shopping with us!\n");
+    printf("------------\n");
+
+    item *current = head;
+    while (current != NULL)
+    {
+        current->quantity -= current->reserved;
+        current->reserved = 0;
+        current = current->next;
+    }
+
+    updateFile();
+
+    printf("Press any key to continue...\n");
+    getchar();
+}
 
 void displayMainMenu()
 {
@@ -262,7 +436,7 @@ void displayMainMenu()
     }
 
     do
-     {
+    {
         clearScreen();
 
         printf("                                      ---------------------------------------------\n");
@@ -280,7 +454,7 @@ void displayMainMenu()
         scanf("%d", &choice);
         getchar();
 
-        switch (choice) 
+        switch (choice)
         {
         case 1:
             addNewItem();
@@ -306,110 +480,7 @@ void displayMainMenu()
         default:
             printf("Invalid choice. Please try again.\n");
         }
-    }
-     while (choice != 7);
-}
-
-void savefile()
-{
-    FILE* file = fopen("items.txt", "r");
-    if (file == NULL)
-    {
-        printf("Error opening file.\n");
-        return;
-    }
-
-    item newItem;
-
-    while (fscanf(file, "%s %s %f %d", newItem.name, newItem.code, &newItem.unitPrice, &newItem.quantity) == 4) {
-        item* newItemPtr = (item*)malloc(sizeof(item));
-        if (newItemPtr == NULL)
-        {
-            printf("Memory allocation failed!\n");
-            return;
-        }
-        strcpy(newItemPtr->name,newItem.name);
-        strcpy(newItemPtr->code,newItem.code);
-        newItemPtr->unitPrice =newItem.unitPrice;
-        newItemPtr->quantity =newItem.quantity;
-        newItemPtr->reserved = 0;
-        newItemPtr->next = head;
-        head = newItemPtr;
-    }
-
-    fclose(file);
-}
-
-void editItem()
-{
-    clearScreen();
-    char code[MAX_CODE];
-    printf("Editing an existing item...\n");
-    printf("Enter Item Code to edit: ");
-    scanf("%s", code);
-
-    item* current = head;
-    while (current!=NULL)
-        {
-        if (strcmp(current->code,code)==0)
-        {
-            printf("Item found! Enter updated details:\n");
-            printf("Enter Item Name: ");
-            scanf("%s", current->name);
-            printf("Enter Unit Price: ");
-            scanf("%f", &current->unitPrice);
-            printf("Enter Item Quantity: ");
-            scanf("%d", &current->quantity);
-            printf("Item updated successfully!\n");
-            printf("Press any key to continue...\n");
-            getchar();
-            getchar();
-            return;
-        }
-        current=current->next;
-    }
-
-    printf("Item is not found!\n");
-    printf("Press any key to continue...\n");
-    getchar();
-}
-
-void deleteItem()
-{
-    clearScreen();
-    char code[MAX_CODE];
-    printf("Deleting an existing item...\n");
-    printf("Enter Item Code to delete: ");
-    scanf("%s",code);
-
-    item* current=head;
-    item* prev=NULL;
-
-    while (current!=NULL)
-        {
-        if (strcmp(current->code,code)==0)
-        {
-            if (prev==NULL)
-            {
-                head=current->next;
-            }
-            else
-            {
-                prev->next=current->next;
-            }
-            free(current);
-            printf("Item with code %s deleted successfully!\n", code);
-            printf("Press any key to continue...\n");
-            getchar();
-            getchar();
-            return;
-        }
-        prev=current;
-        current=current->next;
-    }
-    printf("Item with code %s not found!\n", code);
-    printf("Press any key to continue...\n");
-    getchar();
+    } while (choice != 7);
 }
 
 int main()
